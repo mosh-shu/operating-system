8ea3857dafc59251699e4725e6cc1713eb1b4b03
Shu Sakamoto <shusakamoto1707@gmail.com>
2019-05-25 12:46:28 +0900

    updated approx, mallocing for mythreadid

--- scripts/lock/t2-lock-approx.c
+++ scripts/lock/t2-lock-approx.c
@@ -1,42 +1,56 @@
+/* Threads with enough work to hopefully have something interesting happen. */
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 #include <pthread.h>
 
 #define NUM_THREADS 10
+// #define NUM_THREADS 4
 
-int gcounter = 100000000; /* 1E8 */
+// int gcounter = 100000000; /* 1E8 */
+#define GCOUNTER 10000000
+
+pthread_mutex_t master_lock = PTHREAD_MUTEX_INITIALIZER;
+
+//// Code from Ch. 29 in OSTEP
 
 typedef struct __counter_t
 {
-    int global;                     // global count
-    pthread_mutex_t glock;          // global lock
-    int local[NUMCPUS];             // local count (per cpu)
-    pthread_mutex_t llock[NUMCPUS]; // ... and locks
-    int threshold;                  // update frequency
+    // init: record threshold, init locks,
+    int global;                         // global count
+    pthread_mutex_t glock;              // global lock
+    int local[NUM_THREADS];             // local count (per cpu)
+    pthread_mutex_t llock[NUM_THREADS]; // ... and locks
+    int threshold;                      // update frequency
 } counter_t;
 
-// init: record threshold, init locks, init values
-//       of all local counts and gloval count
-
+// init: record threshold, init locks,
+// global count
+// global lock
+// local count (per cpu)
+// ... and locks
+// update frequency
 void init(counter_t *c, int threshold)
 {
     c->threshold = threshold;
     c->global = 0;
     pthread_mutex_init(&c->glock, NULL);
     int i;
-    for (i = 0; i < NUMCPUS; i++)
+    for (i = 0; i < NUM_THREADS; i++)
     {
         c->local[i] = 0;
         pthread_mutex_init(&c->llock[i], NULL);
     }
 }
 
-// update: usually, just grab local lock and update local amount
-//         once local count has risen by 'threshold', grab gloval
-//         lock and transfer local values to it
+// update: usually, just grab local lock
+// once local count has risen by
+// lock and transfer local values to it
 void update(counter_t *c, int threadID, int amt)
 {
-    int cpu = threadID % NUMCPUS;
+    int cpu = threadID % NUM_THREADS;
+    // printf("in %d: %d\n", threadID, c->local[cpu]);
     pthread_mutex_lock(&c->llock[cpu]);
     c->local[cpu] += amt; // assumes amt > 0
     if (c->local[cpu] >= c->threshold)
@@ -47,26 +61,32 @@ void update(counter_t *c, int threadID, int amt)
         c->local[cpu] = 0;
     }
     pthread_mutex_unlock(&c->llock[cpu]);
+    // printf("out %d: %d\n", threadID, c->local[cpu]);
 }
 
-//get: just return global amount (which may not be perfect)
+// get: just return global amount (which may not be perfect)
 int get(counter_t *c)
 {
     pthread_mutex_lock(&c->glock);
-    int vat = c->global;
+    int val = c->global;
     pthread_mutex_unlock(&c->glock);
-    return val; //only approximate
+    return val; // only approximate!
 }
 
-void *mythread(int *arg)
+///// end of code from OSTEP
+
+counter_t global_counter;
+
+void *mythread(void *arg)
 {
-    int lcounter = gcounter / NUM_THREADS;
+    int lcounter = GCOUNTER / NUM_THREADS;
     int mythreadid = *(int *)arg;
-    printf("%d: start\n", mythreadid;
+    printf("%d: start\n", mythreadid);
+    /* lcounter here is just the local loop -- how many times are we going to loop? */
     while (lcounter)
     {
-        if (get(&global_counter) % 10000000 == 0 && get(&global_counter) > 0)
-            printf("%s: %d\n", (char *)arg, gcounter);
+        if (get(&global_counter) % 1000000 == 0 && get(&global_counter) > 0)
+            printf("%d: lcounter: %d gcounter: %d\n", mythreadid, lcounter, get(&global_counter));
         update(&global_counter, mythreadid, +1); /* call the shared counter's update routine */
         lcounter--;
     }
@@ -78,20 +98,19 @@ int main(int argc, char *argv[])
 {
     pthread_t threads[NUM_THREADS];
     int rc;
-    int threadid = 0;
+    int *threadid;
     printf("main: begin\n");
 
     /* First, initialize our shared counter structure; 1000000 is the "threshold", how often the local values update the global */
-    init(&global_counter, 1000);
+    init(&global_counter, 100);
 
     /* create our threads and fire them off */
     for (int i = 0; i < NUM_THREADS; i++)
     {
-        rc = pthread_create(&threads[i], NULL, mythread, &threadid);
-        assert(rc == 0);
-        threadid++;
+        threadid = malloc(sizeof(int));
+        *threadid = i;
+        rc = pthread_create(&threads[i], NULL, mythread, threadid);
     }
-
     // join waits for the threads to finish
     printf("Okay, main() (our original thread) now waiting...\n");
     for (int i = 0; i < NUM_THREADS; i++)

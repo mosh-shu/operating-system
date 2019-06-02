66a738b00c756124edf241ffbc3d7a12eb25ec6f
Shu Sakamoto <shusakamoto1707@gmail.com>
2019-06-02 18:42:38 +0900

    init lock.sh

---
+++ scripts/lock/lock.sh
@@ -0,0 +1,12 @@
+echo > result.txt
+for i in 1 10 100 1000 10000 100000 1000000 10000000
+do
+echo THRESHOLD:$i >> result.txt
+for j in `seq 10`
+do
+echo trial:$j >> result.txt
+(time ./t2-lock-var.out $i) >> result.txt 2>&1
+echo >> result.txt
+done
+echo "-------" >> result.txt
+done

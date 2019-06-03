86af931654568370cda03277e0b0118761a4772c
Shu Sakamoto <shusakamoto1707@gmail.com>
2019-06-03 19:55:57 +0900

    final run of lock.sh, now echos threads and threshold every time

--- scripts/lock/lock.sh
+++ scripts/lock/lock.sh
@@ -5,8 +5,6 @@ for num_threads in 1 2 #3 4 5 6 7 8 9 10 20 30 40 50 60 70 80 90 100 200 300 400
 do 
 sed -i 's/\#define NUM_THREADS [0-9]*//' t2-lock-var.c
 sed -i '1i#define NUM_THREADS '$num_threads t2-lock-var.c
-echo NUM_THREADS:$num_threads >> result.txt
-echo currently running $num_threads
 
 for threshold in 1 10 #100 1000 10000 100000 1000000 10000000 100000000
 
@@ -14,8 +12,12 @@ do
 
 sed -i 's/\#define THRESHOLD [0-9]*//' t2-lock-var.c
 sed -i '1i#define THRESHOLD '$threshold t2-lock-var.c
-echo NUM_THREADS:$threshold >> result.txt
-echo currently running $threshold
+
+echo NUM_THREADS:$num_threads >> result.txt
+echo THRESHOLD:$threshold >> result.txt
+echo currently running: 
+echo NUM_THREADS:$num_threads
+echo THRESHOLD:$threshold
 
 gcc t2-lock-var.c -o t2-lock-var.out -Wall -pthread
 
